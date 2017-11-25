aa6dd0afc08e8ad49c16231fd3edcc5658b1f352
Scott Lahteine <github@thinkyhead.com>
2017-11-25 16:27:55 -0600

    M290 report on change, P0 to leave Probe Z Offset unchanged

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -9430,6 +9430,14 @@ inline void gcode_M226() {
 
 #if ENABLED(BABYSTEPPING)
 
+  #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
+    FORCE_INLINE void mod_zprobe_zoffset(const float &offs) {
+      zprobe_zoffset += offs;
+      SERIAL_ECHO_START();
+      SERIAL_ECHOLNPAIR(MSG_PROBE_Z_OFFSET ": ", zprobe_zoffset);
+    }
+  #endif
+
   /**
    * M290: Babystepping
    */
@@ -9440,7 +9448,7 @@ inline void gcode_M226() {
           const float offs = constrain(parser.value_axis_units((AxisEnum)a), -2, 2);
           thermalManager.babystep_axis((AxisEnum)a, offs * planner.axis_steps_per_mm[a]);
           #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
-            if (a == Z_AXIS) zprobe_zoffset += offs;
+            if (a == Z_AXIS && parser.boolval('P', true)) mod_zprobe_zoffset(offs);
           #endif
         }
     #else
@@ -9448,13 +9456,9 @@ inline void gcode_M226() {
         const float offs = constrain(parser.value_axis_units(Z_AXIS), -2, 2);
         thermalManager.babystep_axis(Z_AXIS, offs * planner.axis_steps_per_mm[Z_AXIS]);
         #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
-          zprobe_zoffset += offs;
+          if (parser.boolval('P', true)) mod_zprobe_zoffset(offs);
         #endif
       }
-    #endif
-    #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
-      SERIAL_ECHO_START();
-      SERIAL_ECHOLNPAIR(MSG_PROBE_Z_OFFSET ": ", zprobe_zoffset);
     #endif
   }
 

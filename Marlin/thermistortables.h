a662e4d3ccf2d43ab28a4894f4d3b006239b3b06
Dami <dami@creabox.net>
2018-09-27 21:05:02 +0200

    Bed Leveling

--- Marlin/thermistortables.h
+++ Marlin/thermistortables.h
@@ -86,6 +86,9 @@
 #if ANY_THERMISTOR_IS(15) // JGAurora A5 thermistor calibration
   #include "thermistortable_15.h"
 #endif
+#if ANY_THERMISTOR_IS(17) // DiscoEasy
+  #include "thermistortable_17.h"
+#endif
 #if ANY_THERMISTOR_IS(20) // PT100 with INA826 amp on Ultimaker v2.0 electronics
   #include "thermistortable_20.h"
 #endif
